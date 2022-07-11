5cfd00d1f985bd7026acfba4b3736cbee33512f3
John Carroll <johndavidcarroll@gmail.com>
2022-07-10 22:18:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(cards, event) {
+    let newArray = []
+    for (let i = 0; i < cards.length; i++) {
+      newArray.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    
+    }
+return newArray
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+      }
+}
